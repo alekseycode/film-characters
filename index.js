27b6e2945858a7e497a3838b7f4f6708b98cc6d9
Aleksey Chernyakhovskiy <98059176+alekseycode@users.noreply.github.com>
2023-01-31 15:35:24 -0500

    Add Server files

---
+++ index.js
@@ -0,0 +1,45 @@
+/*
+    Imports
+*/
+const express = require('express');
+const app = express();
+// Environment variables
+require('dotenv').config();
+// Logging any requests w/ colorized status codes
+const logger = require("morgan");
+// Connection to database
+const connectToMongoDB = require("./db/mongodb");
+// Prevent CORS issue
+const cors = require('cors');
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    optionSuccessStatus: 200
+}
+
+/*
+    Middleware
+*/
+app.use(logger("dev"));
+app.use(cors(corsOptions));
+// Read incoming requests property
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+/*
+    Routes
+*/
+const mcuRouter = require("./routes/mcuRouter");
+app.use("/api", mcuRouter);
+
+/*
+    Server listening
+*/
+// const PORT = 3001;
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+
+    connectToMongoDB();
+})
